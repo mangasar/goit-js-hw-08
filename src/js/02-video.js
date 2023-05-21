7cd7b5908bb508cb91676c479d1f376198d2237c
Mangasar <mangasarpetrosyan@gmail.com>
2023-05-21 20:39:13 +0300

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
 
+    player.on('timeupdate', throttle(onPlay, 1000));
+
+
+    function onPlay(data) {
+        localStorage.setItem(
+          'videoplayer-current-time',
+          JSON.stringify(data.seconds)
+        );
+      }
+
+      const currentTime = localStorage.getItem('videoplayer-current-time');
+
+      if (currentTime !== null) {
+        player.setCurrentTime(currentTime);
+      }
