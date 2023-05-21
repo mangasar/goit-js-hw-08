1b4795277df99b1566037b6dd8c02f51ba84321c
mangasar <92088655+mangasar@users.noreply.github.com>
2023-05-21 18:08:13 +0000

    Deploying to gh-pages from @ mangasar/goit-js-hw-08@bfd6f24abad484bb89950deaf07b07a45bce12a2 🚀

--- 02-video.ad626607.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),t=new Vimeo.Player(e);t.on("timeupdate",throttle((function(e){localStorage.setItem("videoplayer-current-time",JSON.stringify(e.seconds))}),1e3));var r=localStorage.getItem("videoplayer-current-time");null!==r&&t.setCurrentTime(r)}();
-//# sourceMappingURL=02-video.ad626607.js.map
